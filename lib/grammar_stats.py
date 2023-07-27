63dd9d024954e65d15c0bea8d21e66cbfc98964b
Alexander Wilson <apwilson97@gmail.com>
2023-07-27 11:50:45 +0100

    Completed the grammar stats challenge for phase two

--- lib/grammar_stats.py
+++ lib/grammar_stats.py
@@ -1,6 +1,9 @@
+import math
+
 class GrammarStats:
     def __init__(self):
-        pass
+        self.passed_checks = 0
+        self.total_checks = 0
   
     def check(self, text):
         # Parameters:
@@ -9,6 +12,7 @@ class GrammarStats:
         #   bool: true if the text begins with a capital letter and ends with a
         #         sentence-ending punctuation mark, false otherwise
         if text == '':
+            self.total_checks += 1
             return False
         
         start_of_text = text[0]
@@ -16,12 +20,15 @@ class GrammarStats:
         punctuation = ['.', '!', '?']
 
         if start_of_text.upper() == start_of_text and end_of_text in punctuation:
+            self.passed_checks += 1
+            self.total_checks += 1
             return True
         else:
+            self.total_checks += 1
             return False
   
     def percentage_good(self):
         # Returns:
         #   int: the percentage of texts checked so far that passed the check
         #        defined in the `check` method. The number 55 represents 55%.
-        pass
+        return math.ceil((self.passed_checks / self.total_checks) * 100)
--- tests/test_grammar_stats.py
+++ tests/test_grammar_stats.py
@@ -25,3 +25,36 @@ def test_given_empty_string():
     grammar_data = GrammarStats()
     result = grammar_data.check('')
     assert result == False
+
+def test_given_string_with_space():
+    grammar_data = GrammarStats()
+    result = grammar_data.check(' ')
+    assert result == False
+
+"""Given 4 different strings, 2 are valid and 2 are not, should return an integer of 50 representing percentage"""
+
+def test_percentage_good_of_fifty():
+    grammar_data = GrammarStats()
+    grammar_data.check('This is a valid string!')
+    grammar_data.check('this is not a valid string')
+    grammar_data.check('this is not a valid string')
+    grammar_data.check('This should be a valid string.')
+    assert grammar_data.percentage_good() == 50
+
+def test_percentage_good_of_51():
+    grammar_data = GrammarStats()
+    valid_strings = ['This is a valid string!' for i in range(1,27)]
+    invalid_strings = ['this is not a valid string' for i in range(1,26)]
+    for string in valid_strings:
+        grammar_data.check(string)
+    
+    for string in invalid_strings:
+        grammar_data.check(string)
+    
+    assert grammar_data.percentage_good() == 51
+
+
+"""For checking the percentage of texts that have passed the check method, could create an instance variable
+where it keeps track of the number of the texts that have passed and also a total number of texts checked
+assigned to another instance variable
+For the test method, we can call the check method multiple times and then call the percentage_good method at the end"""
